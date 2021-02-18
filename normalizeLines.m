41517c78649ff95e410be3fdb1d20726b7b8e7ff
Susanne K. Suter <susanne.suter@gmail.com>
2021-02-18 11:15:35 +0100

    Update normalizeLines.m

--- normalizeLines.m
+++ normalizeLines.m
@@ -1,6 +1,10 @@
 function out = normalizeLines(lines)
 % normalize lines to inverval between 0 an 1, output contains only values
 % of lines != size of lines
+
+%Copyright (c) 2021 University of Zurich, Institute of Evolutionary Medicine
+
+
 out = [];
 y_length = size(lines, 1);
 for l = 1:y_length
