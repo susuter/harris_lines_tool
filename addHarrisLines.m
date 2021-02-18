cf2b2b48118acbef5eadeb85872034b8c8b135b9
Susanne K. Suter <susanne.suter@gmail.com>
2021-02-18 11:03:53 +0100

    Update addHarrisLines.m

--- addHarrisLines.m
+++ addHarrisLines.m
@@ -2,7 +2,7 @@ function addHarrisLines()
 %user may manually add harris lines by clicking on left subplot. the
 %result is schon on the right subplot image.
 
-
+%Copyright (c) 2021 University of Zurich, Institute of Evolutionary Medicine
 
 user_data =  get(gcf, 'UserData');
 
