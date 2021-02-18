034f6dd5a04c82cd37e5def91c36a228afaaf7dd
Susanne K. Suter <susanne.suter@gmail.com>
2021-02-18 11:04:52 +0100

    Update byers_age_lookup_female.m

--- byers_age_lookup_female.m
+++ byers_age_lookup_female.m
@@ -1,6 +1,8 @@
 function age = byers_age_lookup_female(hl)
 %hl = percentage of occurence of harris line in distal adult diaphyses
 
+%Copyright (c) 2021 University of Zurich, Institute of Evolutionary Medicine
+
 
 table = [31.5, 1; 40.1,2; 46.6, 3; 52.4, 4; 58.6, 5; 63.9, 6; 69.0, 7; ...
     74.3, 8; 79.4, 9; 83.9, 10; 88.8, 11; 93.0, 12; 96.5, 13; 98.3, 14; ...
