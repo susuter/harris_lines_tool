9103941ef9ad7a27077fb8422b32dbbc3a669573
Susanne K. Suter <susanne.suter@gmail.com>
2021-02-18 11:05:59 +0100

    Update clarke_age_lookup_male.m

--- clarke_age_lookup_male.m
+++ clarke_age_lookup_male.m
@@ -1,6 +1,9 @@
 function age = clarke_age_lookup_male(hl)
 %hl = percentage of occurence of harris line in distal adult diaphyses
 
+%Copyright (c) 2021 University of Zurich, Institute of Evolutionary Medicine
+
+
 table = [0.423, 0; 0.347, 1; 0.296, 2; 0.265, 3;0.240, 4; 0.219, 5; ...
     0.200, 6; 0.178, 7; 0.160, 8; 0.141, 9; 0.120, 10; 0.101, 11; ...
     0.084, 12; 0.068, 13; 0.046, 14; 0.032, 15; 0.016, 16];
